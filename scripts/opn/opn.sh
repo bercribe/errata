677b9691ec43719f554ba82dd193f7fca67c67d3
mawz <mawz@hey.com>
2026-06-30 17:44:15 -0700

    fix opn on ssh

--- scripts/opn/opn.sh
+++ scripts/opn/opn.sh
@@ -1,2 +1,2 @@
-opener=${OPENER:-$(command -v xdg-open || command -v open)}
+opener=${OPENER:-$(command -v xdg-open || command -v open || command -v echo)}
 for f in "$@"; do $opener "$f" 2>/dev/null || echo "$f"; done
