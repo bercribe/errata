31201c68f32554a24e48a89b61730a5784e66615
mawz <mawz@hey.com>
2026-09-14 09:32:50 -0700

    increase expiry

--- scripts/notification/notify.sh
+++ scripts/notification/notify.sh
@@ -3,7 +3,7 @@ title="${1:-Notification}"
 description="${2:-$(date --iso-8601=seconds)}"
 
 if hash notify-send 2>/dev/null; then
-  exec notify-send --expire-time=5000 "$title" "$description"
+  exec notify-send --expire-time=60000 "$title" "$description"
 elif hash osascript 2>/dev/null; then
   exec osascript -e "display notification \"$description\" with title \"$title\""
 else
